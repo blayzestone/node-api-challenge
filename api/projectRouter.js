bedc5a105b045dd17fb79922374a61ec4a696b81
Blayze <blayzestone@protonmail.com>
2020-06-05 09:13:37 -0700

    rename projectDb to db

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -1,43 +1,43 @@
 const router = require("express").Router();
-const projectDb = require("../data/helpers/projectModel");
+const db = require("../data/helpers/projectModel");
 
 module.exports = router;
 
 router.get("/", (req, res) => {
-  projectDb.get()
+  db.get()
     .then((projects) => res.status(200).json(projects))
     .catch((err) => console.log(err));
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  projectDb.get(Number(id))
+  db.get(Number(id))
     .then((projects) => res.status(200).json(projects))
     .catch((err) => console.log(err));
 });
 
 router.get("/:id/actions", (req, res) => {
   const { id } = req.params;
-  projectDb.getProjectActions(Number(id))
+  db.getProjectActions(Number(id))
     .then((actions) => res.status(200).json(actions))
     .catch((err) => console.log(err));
 });
 
 router.post("/", (req, res) => {
-  projectDb.insert(req.body)
+  db.insert(req.body)
     .then((project) => res.status(201).json(project))
     .catch((err) => console.log(err));
 });
 
 router.patch("/:id", (req, res) => {
   const { id } = req.params;
-  projectDb.update(Number(id), req.body)
+  db.update(Number(id), req.body)
     .then((project) => res.status(200).json(project))
     .catch((err) => console.log(err));
 });
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  projectDb.remove(Number(id))
+  db.remove(Number(id))
     .then((result) => res.status(201).json(result));
 });
