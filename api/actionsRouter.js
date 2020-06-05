32d164efe0e0d77fcb81b658168358a3252fb192
Blayze <blayzestone@protonmail.com>
2020-06-05 09:22:00 -0700

    add endpoint to create actions to /api/actions

--- api/actionsRouter.js
+++ api/actionsRouter.js
@@ -1,4 +1,18 @@
 const router = require("express").Router();
-const actionDB = require("../data/helpers/projectModel");
+const db = require("../data/helpers/actionModel");
 
 module.exports = router;
+
+router.post("/:id", (req, res) => {
+  const { id } = req.params;
+  const action = {
+    ...req.body,
+    project_id: Number(id),
+  }
+
+  console.log(action);
+
+  db.insert(action)
+    .then((action) => res.status(201).json(action))
+    .catch((err) => console.log(err));
+});
